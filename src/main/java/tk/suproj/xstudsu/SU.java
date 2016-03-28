c91b8364a2329893adc23f1ad03b1e84ba226cdb
mob41 <mob41@users.noreply.github.com>
2016-03-28 13:17:59 +0800

    check installation every patch instead public variable.

--- src/main/java/tk/suproj/xstudsu/SU.java
+++ src/main/java/tk/suproj/xstudsu/SU.java
@@ -7,12 +7,51 @@ import tk.suproj.xstudsu.ui.UI;
 
 public class SU {
 	
-	public static boolean ver32_64 = false;
-	public static boolean patched = false;
+	private static boolean ver32_64 = false;
+	private static boolean patched = false;
 	private static final String path32 = "C:\\Program Files (x86)\\Sun-Tech\\XCLASS EVO\\student";
 	private static final String path64 = "C:\\Program Files\\Sun-Tech\\XCLASS EVO\\student";
 	private static String path;
 	
+	public static boolean isPatched(){
+		switch (checkInstall()){
+		case 3:
+		case 4:
+			patched = true;
+			return true;
+		default:
+			patched = false;
+			return false;
+		}
+	}
+	
+	/***
+	 * Get the version of XCLASS EVO is 32 or 64 bit.<br>
+	 * Return integer 32 if 32bit, 64 if 64bit.
+	 * @return 32 / 64 bit
+	 */
+	public static int getVer32_64bit(){
+		switch (checkInstall()) {
+		case 1:
+		case 3:
+			return 32;
+		case 2:
+		case 4:
+			return 64;
+		default:
+			return -1;
+		}
+	}
+	
+	/***
+	 * Check installation of XCLASS EVO<br>
+	 * Return <b>1</b> if NOT patched and 32 bit.<br>
+	 * Return <b>2</b> if NOT patched and 64 bit.<br>
+	 * Return <b>3</b> if PATCHED and 32 bit.<br>
+	 * Return <b>4</b> if PATCHED and 64 bit.<br>
+	 * Return <b>0</b> if XCLASS EVO could not be found.
+	 * @return a integer indicating the installation status of XCLASS
+	 */
 	public static int checkInstall(){
 		File file = new File(path32 + "\\xstudent.exe");
 		if(file.exists()){
@@ -41,6 +80,10 @@ public class SU {
 		return 0;
 	}
 	
+	/***
+	 * Unpatch / start the XCLASS EVO application (xstudent.exe)
+	 * @return whether the application started successfully via XstudSu
+	 */
 	public static boolean start(){
 		UI.logs.append("NOTE: Starting xstudent...\n");
 		if(ver32_64){
@@ -85,6 +128,10 @@ public class SU {
 		return true;
 	}
 	
+	/***
+	 * Patch / stop the XCLASS EVO application (xstudent.exe)
+	 * @return whether the application stopped successfully via XstudSu.
+	 */
 	public static boolean stop(){
 		UI.logs.append("NOTE: Stopping xstudent...\n");
 		if(ver32_64){
